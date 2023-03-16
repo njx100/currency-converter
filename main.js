e6a19473c8fc5280d0e929fef8524903d20837e2
njx100 <dnnguyen100@gmail.com>
2023-03-16 17:19:37 +0700

    js initiate code

---
+++ main.js
@@ -0,0 +1,5 @@
+const currencyFirstEl = document.getElementById("currency-first");
+const currencySecondEl = document.getElementById("currency-second");
+const worthFirstEl = document.getElementById("worth-first");
+const worthSecondEl = document.getElementById("worth-second");
+const exchangeRateEl = document.getElementById("exchange-rate");
