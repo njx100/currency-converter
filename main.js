e0cead16fb53b536df4cb5c27c05d23dfe80feb2
njx100 <dnnguyen100@gmail.com>
2023-03-17 15:48:50 +0700

    css need to fix

--- main.js
+++ main.js
@@ -5,21 +5,20 @@ const worthSecondEl = document.getElementById("worth-second");
 const exchangeRateEl = document.getElementById("exchange-rate");
 const apiURL = "https://v6.exchangerate-api.com/v6/3013f9ff40e41b4e3ed23c4d/latest/";
 
-async function updateRate() {
-    const api = apiURL + currencyFirstEl.value;
-    const data = await fetch(api).then((res) => res.json());
-    const firstCurrency = currencyFirstEl.value;
-    const secondCurrency = currencySecondEl.value;
-    
-    // console.log(firstCurrency);
-    // console.log(secondCurrency)
-    // console.log(data.conversion_rates[firstCurrency]);
-    // console.log(data.conversion_rates[secondCurrency]);
-    worthSecondEl.value = worthFirstEl.value * (data.conversion_rates[secondCurrency]);
-    
+async function updateRate() { 
+    try {
+        const api = apiURL + currencyFirstEl.value;
+        const data = await fetch(api).then((res) => res.json());
+        const firstCurrency = currencyFirstEl.value;
+        const secondCurrency = currencySecondEl.value;
+        worthSecondEl.value = (worthFirstEl.value * (data.conversion_rates[secondCurrency])).toFixed(4);
+        exchangeRateEl.innerText = `${worthFirstEl.value} ${firstCurrency} = ${worthSecondEl.value} ${secondCurrency}`; 
+    } catch (error) {
+        exchangeRateEl.innerText = "An error happened, try again later!"
+    }
 }
 
-
+updateRate();
 currencyFirstEl.addEventListener("change", updateRate);
 currencySecondEl.addEventListener("change", updateRate);
 worthFirstEl.addEventListener("change", updateRate);
